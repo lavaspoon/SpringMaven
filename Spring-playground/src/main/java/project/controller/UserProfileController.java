a7ccfe112996eb11140a9ba62a32a64a7beaab54
lavaspoon <lavaspoon@naver.com>
2021-11-18 16:53:57 +0900

    코드정리 및 주석

--- Spring-playground/src/main/java/project/controller/UserProfileController.java
+++ Spring-playground/src/main/java/project/controller/UserProfileController.java
@@ -10,11 +10,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@RestController //스프링 프레임워크가 클래스를 컨트롤러로 인식하고 인스턴스를 생성
 public class UserProfileController {
-    private Map<String, UserProfile> userMap;
+    private Map<String, UserProfile> userMap; // < ID(key), UserProfile(value) >
 
-    @PostConstruct
+    @PostConstruct //스프링 프레임워크가 클래스 인스턴스를 만들고 그 직후에 호출함
     public void init(){
         userMap = new HashMap<String, UserProfile>();
         userMap.put("1", new UserProfile("1","홍길동","010-1234-1234","대전시 서구"));
@@ -32,17 +32,24 @@ public class UserProfileController {
         return new ArrayList<UserProfile>(userMap.values());
     }
 
+    //데이터 생성
     @PutMapping("/user/{id}")
-    public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
+    public void putUserProfile(@PathVariable("id") String id,
+                               @RequestParam("name") String name,
+                               @RequestParam("phone") String phone,
+                               @RequestParam("address") String address)
+    {
         UserProfile userProfile = new UserProfile(id,name,phone,address);
         userMap.put(id, userProfile);
     }
 
+    //데이터 수정
     @PostMapping("/user/{id}")
     public void postUserProfile(@PathVariable("id") String id,
                                 @RequestParam("name") String name,
                                 @RequestParam("phone") String phone,
-                                @RequestParam("address") String address) {
+                                @RequestParam("address") String address)
+    {
         UserProfile userProfile = userMap.get(id);
         userProfile.setName(name);
         userProfile.setPhone(phone);
