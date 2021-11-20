f3f78b5a67af94160dc783a16869f582b68230eb
lavaspoon <lavaspoon@naver.com>
2021-11-21 00:45:01 +0900

    전체조회, 상세조회 구현 (+ 수정 기능 추가 필요)

---
+++ Spring-playground/src/main/java/project/Springplayground/controller/BasicController.java
@@ -0,0 +1,51 @@
+package project.Springplayground.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import project.Springplayground.mapper.UserProfileMapper;
+import project.Springplayground.model.UserProfile;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    private UserProfileMapper mapper;
+
+    public BasicController(UserProfileMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    //회원가입
+    @GetMapping("/joinForm")
+    public String addForm(){
+        return "/basic/joinForm";
+    }
+
+    @PostMapping("/joinForm")
+    public String addUser(@RequestParam String name,
+                          @RequestParam String phone,
+                          @RequestParam String address)
+    {
+        mapper.insertUserProfile(name, phone, address);
+        return "searchUser";
+    }
+
+    //회원 전체 조회
+    @GetMapping("/searchUser")
+    public String searchUser(Model model){
+        List<UserProfile> userProfileList = mapper.getUserProfileList();
+        model.addAttribute("items", userProfileList);
+        return "basic/searchUser";
+    }
+    //회원 상세 조회
+    @GetMapping("/items/{id}")
+    public String item(@PathVariable String id, Model model) {
+        UserProfile userProfile = mapper.getUserProfile(id);
+        model.addAttribute("item", userProfile);
+        return "basic/userInform";
+    }
+
+}
