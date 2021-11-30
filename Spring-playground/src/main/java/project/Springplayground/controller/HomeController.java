d991724c58e857bc46d372fb820590b01be7c21d
lavaspoon <lavaspoon@naver.com>
2021-11-30 17:45:10 +0900

    로그인 - 홈화면 생성

---
+++ Spring-playground/src/main/java/project/Springplayground/controller/HomeController.java
@@ -0,0 +1,15 @@
+package project.Springplayground.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
